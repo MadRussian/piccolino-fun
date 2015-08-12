f493fb9bb3c5961ec2f59de4761ad81ca13bd3fe
Nodar Nutsubidze <nnutsubidze@ecessa.com>
2015-08-11 20:18:48 -0500

    Introduction of my fun tests
    - blue_red_pill: Depending on which sensor is pressed different matrix
      style text is printed
    - text_size_change: Use the touch sensors to change the size of the text

---
+++ README.md
@@ -0,0 +1,6 @@
+Piccolino Fun
+=============
+
+This contains various mini projects that I've worked on with the [Piccolino](http://www.piccolino.rocks/)
+
+All of these projects used Arduino IDE
---
+++ blue_red_pill/blue_red_pill.ino
@@ -0,0 +1,58 @@
+#include <Wire.h>
+#include <CapacitiveSensor.h>
+#include <Piccolino_OLED.h>
+/**
+ * Based on which sensor is pressed this shows different messages
+ */
+
+Piccolino_OLED     screen;
+CapacitiveSensor   tleft = CapacitiveSensor(2,3);
+CapacitiveSensor   tright = CapacitiveSensor(2,4);
+
+#define TRIGGER_LEVEL 100// change as appropriate
+
+void setup()
+{
+   Serial.begin(9600);
+   screen.begin();
+   delay(100);
+   Serial.print(F("Go ahead, touch my face-plate...\n"));
+   screen.setTextSize(3);
+}
+
+/**
+ * Whether a sensor is being touched
+ */
+bool touched(long sensor)
+{
+  if(sensor > TRIGGER_LEVEL) {
+    return true;
+  }
+  return false;
+}
+
+void loop()
+{
+    long start = millis();
+    long sen_left =  tleft.capacitiveSensor(30)/10;
+    long sen_right =  tright.capacitiveSensor(30)/10;
+
+    screen.clear();
+    screen.setTextSize(2);
+    screen.setCursor(0,0);
+
+    if(touched(sen_left) && touched(sen_right)) {
+      screen.setTextSize(1);
+      Serial.print(F("BOTH\n"));
+      screen.print(F("STOP IT!!\nOnly one at a time!\nUnless you want the\nMadRussian to come\nafter you\n"));
+    } else if(touched(sen_left)) {
+      Serial.print(F("LEFT\n"));
+      screen.print(F("Red Pill\n"));
+    } else if(touched(sen_right)) {
+      Serial.print(F("RIGHT\n"));
+      screen.print(F("Blue Pill\n"));
+    } else {
+      screen.print(F("Touch my\nsensors!!!"));
+    }
+    screen.update();
+}
